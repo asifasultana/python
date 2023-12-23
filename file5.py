2f744dd20416b11283da187e6a9b621277863f99
asifasultana <asifasultana117@gmail.com>
2023-12-23 21:28:30 +0500

    complex and boolean data type

---
+++ file5.py
@@ -0,0 +1,48 @@
+#complex data type
+a = 5 + 4j
+print(type(a))
+# boolean data type
+x = True
+print(type(x))
+
+
+
+print(20<40)
+
+print(20>80)
+    
+    
+    
+x = bool(20)
+print(x)
+
+x = bool("pakistan") # non emty string is true
+print(x)
+
+x = bool("") # emty string gives false
+print(x)
+
+x =  bool(0)  # 0 gives false
+print(x)
+
+x = bool(None) # None gives false
+print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
