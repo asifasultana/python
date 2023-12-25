702f9f2ea073e23f23216c8e75bd352a8131b92e
asifasultana <asifasultana117@gmail.com>
2023-12-25 08:09:37 +0500

    python operators

---
+++ file7.py
@@ -0,0 +1,81 @@
+# arithmetic operators
+x = 30
+y = 27
+print(x+y) # addition
+print(x-y) # subs
+print(x*y) # mul
+print(x/y) # div
+print(x%y) # mod (remainder)
+print(x**y)  # exponential power
+print(x//y) # floor division
+
+# comparison operator
+x = y #assign operator
+print(x == y) #equal
+print(x != y) #not equal
+x = 100
+y = 100
+print(x >= y) 
+print(x > y)
+
+# logical operators
+# exp1 (log) exp2
+#and
+x = 40
+y = 50 
+z = 60
+print(x<y and z<100) # only true,true is true othereise false 
+print(True and True)
+print( False and True)
+
+
+#or
+# only false,false is false otherwise true
+print( x < y or z > 100 )
+print(x > y or z > 100 )
+
+#not 
+# reverse ( true become false and false become true)
+print(True)
+print(not True)
+
+print(10>5)
+print(not(10>5))
+
+#assignment operator
+x = 100
+y = 200
+x +=y  #x = x + y
+print(x)
+#https://www.w3schools.com/python/gloss_python_assignment_operators.asp
+
+#identity operators
+
+x = 100
+y = 100
+print ( x is y)
+
+y = 200
+print(x is y) #is
+
+print(x is not y) #isnot
+
+
+
+x = "pakistani flag"
+y = "flag"
+print(x in y) #in
+print(y in x)
+print (x is not y) #is not
+
+
+
+
+
+
+
+
+
+
+
+
